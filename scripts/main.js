627375d47305d746ae632158b7d5fd9f43769574
zred <zachary.redmond@gmail.com>
2024-06-02 14:13:22 -0500

    js improvements / add search

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,9 @@
 import { renderTable } from './renderTable.js';
 import { initializeToggleTable } from './toggleTable.js';
+import { addSearchFunctionality } from './search.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   initializeToggleTable();
   renderTable();
+  addSearchFunctionality();
 });
